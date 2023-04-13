c4078302dbac5a56258e8e07d73203fd5a04616c
philkatie <kaphillips219@gmail.com>
2023-04-13 14:12:14 -0500

    lol forgot to add files

---
+++ src/components/GameOver.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+
+function GameOver() {
+    const { gameOver, correctWord, currentAttempt } = useContext(AppContext);
+    return (
+        <div className="gameOver">
+            <h3>{gameOver.guessedWord ? "You Win!" : "You Lose :("}</h3>
+            <h1>Correct Word: {correctWord}</h1>
+            {gameOver.guessedWord && (<h3>You guessed in {currentAttempt.attempt} attempts.</h3>)}
+        </div>
+    );
+}
+
+export default GameOver
